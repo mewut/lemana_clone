d5832de65d63e48fd86077e04812a0444afe8a5c
Name <shi-666@yandex.ru>
2024-12-25 20:51:24 +0300

    refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
-import axios from 'axios';
 import ProductList from './components/ProductList/ProductList';
 
 const App = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/products/')
-      .then(response => {
-        setProducts(response.data);
-      })
-      .catch(error => {
-        console.error('There was an error fetching the products!', error);
-      });
-  }, []);
-
   return (
     <div className="App">
-      <ProductList products={products} />
+      <ProductList />
     </div>
   );
 };
