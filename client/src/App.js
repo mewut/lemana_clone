910d44b5672ed06cf15a403219294f8425406593
Name <shi-666@yandex.ru>
2024-12-30 23:47:42 +0300

    feat: logo slider

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-// import Header from './components/Header/Header';
+import Header from './components/Header/Header';
 import Body from './components/Body/Body';
 // import Footer from './components/Footer/Footer';
 
@@ -8,7 +8,7 @@ import Body from './components/Body/Body';
 const App = () => {
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       <Body />
       {/* <Footer /> */}
       
